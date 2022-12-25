2b9a44382c97078838f053b0ff8f2d55dd666663
Muhammad Rafli Saputra <112410023+rafli0409@users.noreply.github.com>
2022-12-25 16:18:21 +0700

    landing_page

---
+++ index.js
@@ -0,0 +1,88 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav_link, we remove the show-menu class
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== Contact EmailJS ====================*/
+function SendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    phone: document.getElementById("phone").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_mjhbsih", "template_7o0rddj", params)
+    .then(function (res) {
+      //   (document.getElementById("name").value = ""),
+      //     (document.getElementById("phone").value = ""),
+      //     (document.getElementById("email").value = ""),
+      //     (document.getElementById("message").value = ""),
+      //     console.log(res);
+      alert("Your Message Sent Successfully");
+    })
+    .catch((err) => console.log(err));
+}
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav_menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 200,
+  //     reset: true
+});
+
+sr.reveal(
+  ".home_data, .about_img, .skills_subtitle, .skills_text, .project_text",
+  {}
+);
+sr.reveal(".home_img, .about_subtitle, .about_text, .skills_img", {
+  delay: 400,
+});
+sr.reveal(".home_social-icon", { interval: 200 });
+sr.reveal(".skills_data, .project_img, .contact_input, .contact_button", {
+  interval: 200,
+});
